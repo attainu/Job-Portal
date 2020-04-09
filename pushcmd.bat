74a5c3acde72bc74c53227e0a1acc03cc1a2842d
Harish Ponna <harishponna55@gmail.com>
2020-04-09 19:01:24 +0530

    Project Almost Accomplished

--- pushcmd.bat
+++
@@ -1,3 +0,0 @@
-git add C:\Users\AGB\Desktop\Project\Job-Portal
-git commit -m"Porject's last stage done"
-git push -u origin dev
--- sample.js
+++
@@ -1,4 +0,0 @@
-var bcrypt = require("bcryptjs")
-
-bcrypt.hash("40627166",10)
-.then(res=>console.log(res))
